7b3bb9d6e92df8d46570eb9e7dacd59ffcf2e5d7
Allen Boyd Cunningham <cleverpiggy1@mac.com>
2021-07-31 09:40:23 -0700

    Replace alerts with nice tables and flashing aninimation to visualize the fight. Better random opponent field generator. Mild refactoring.

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
-const FIELDS = ["attack", "defense", "hps", "speed"];
-const TROOPS = 25;
+const ATTRIBUTES = ["attack", "defense", "hps", "speed"];
+const POOL = 25;
+
+
+// ---------------------------- game logic ------------------------------------------
 
 function play(p1, p2) {
 
@@ -8,33 +11,46 @@ function play(p1, p2) {
         return;
     }
     const me = extractStats();
+
+    if (me.name == "") {
+        alert("Enter a name please!");
+        return;
+    }
     const them = getOpponentStats();
 
-    switch (fight(me, them)) {
-        case 0: alert(`${me.name} wins!`);
-        break;
-        case 1: alert(`${them.name} wins!`);
-        break;
-        default: alert(`You've got the same allocations!  Try again.`);
-        break;
+    showGame(me, them);
+    const actionSeq = fight(me, them);
+    if (!actionSeq) {
+        alert("You have the same allocations!  Try again.");
+        return;
     }
-}
+    const delay = timeout(1500, 1000);
+    delay(animateOrder, [actionSeq[0].attacker, actionSeq[0].defender]);
+    for (let attack of actionSeq) {
+        delay(animateAttack, [attack]);
+    }
 
+    // The guy who attacked last obviously won.
+    const winner = actionSeq.pop().attacker;
+    delay(animateWin, [winner]);
+}
 
 // p1, p2: stat field of each player
 // Assumes each player passed validate.
-// returns -> winner {0, 1, 2}
-//   where 0: p1, 1: p2, 2: identical stats is a tie
+// returns -> the action sequence of the fight
 function fight(p1, p2) {
     if (symetric(p1, p2)) {
-        return 2;
+        return null;
     }
+
     const order = [null, null];
+    var flip = false;
     if (p1.speed == p2.speed) {
-        const first = flip_coin();
+        const first = flipCoin();
         const second = {1:0, 0:1}[first];
         order[first] = p1;
         order[second] = p2;
+        flip = true;
     } else if (p1.speed > p2.speed) {
         order[0] = p1;
         order[1] = p2;
@@ -42,21 +58,21 @@ function fight(p1, p2) {
         order[0] = p2;
         order[1] = p1;
     }
-    animateOrder(...order);
+
+    // push each attack to animate later
+    const actionSeq = [];
 
     while (p1.hps > 0 && p2.hps > 0) {
-        attack(...order);
+        console.log('attacking');
+        actionSeq.push(attack(...order));
         order.reverse();
     }
-    if (p1.hps > 0) {
-        return 0;
-    } else {
-        return 1;
-    }
+
+    return actionSeq;
 }
 
 function symetric(p1, p2) {
-    for (field of FIELDS) {
+    for (let field of ATTRIBUTES) {
         if (p1[field] != p2[field]) {
             return false;
         }
@@ -65,34 +81,131 @@ function symetric(p1, p2) {
 }
 
 // Modifies defender by the amount of damage he has taken.
+// Returns an object {attacker, defender, damage}
 function attack(attacker, defender) {
     const dmg = Math.max(attacker.attack - defender.defense, 1);
     defender.hps = Math.max(defender.hps - dmg, 0);
-    animateAttack(attack, defender, dmg);
+    return {
+        attacker: clone(attacker),
+        defender: clone(defender),
+        damage: dmg,
+    };
 }
+
+function getOpponentStats() {
+    return randomFoe();
+}
+
+function randomFoe() {
+    const output = {name: "foe", id: "villian", attack: 1, defense: 0, hps: 1, speed: 0};
+    // Each position corresponds the same position in ATTRIBUTES.
+    const ratios = ATTRIBUTES.map(_ => Math.random());
+    const tot = ratios.reduce((a, b) => a + b);
+
+    var available = POOL - 2; // Subtract the mandatory 1 hp and 1 attack.
+    const values = ratios.map(r => Math.round(available * r / tot));
+    for (let i = 0; i < ATTRIBUTES.length; i++) {
+        let field = ATTRIBUTES[i];
+        let value = values[i];
+
+        // It's possible we don't end up with the exact number depending
+        // on how the rounding went so I'll check the available pool at
+        // each step and stop assigning when it hits zero.
+        if (value >= available) {
+            output[field] += available;
+            break;
+        }
+        output[field] += value;
+        available -= value;
+    }
+    return output;
+}
+
+// ------------------------------ ui / display ----------------------------------
 
 function animateOrder(first, second) {
     if (first.speed == second.speed) {
         console.log(`Equal speeds, ${first.name} wins the flip`);
-        alert(`Equal speeds, ${first.name} wins the flip`);
+        attackMessage(`Equal speeds, ${first.name} wins the flip`);
     } else {
         console.log(`${first.name} goes first`);
-        alert(`${first.name} goes first`);
+        attackMessage(`${first.name} goes first`);
     }
 }
 
-function animateAttack(attacker, defender, dmg) {
-    console.log(`${attacker.name} deals ${dmg} damage.  ${defender.name}'s hps are reduced to ${defender.hps}`);
-    alert(`${attacker.name} deals ${dmg} damage.  ${defender.name}'s hps are reduced to ${defender.hps}`);
+// attack {attacker, defender, damage}
+function animateAttack(attk) {
+    console.log(`${attk.attacker.name} deals ${attk.damage} damage.  ${attk.defender.name}'s hps are reduced to ${attk.defender.hps}`);
+    attackMessage(`${attk.attacker.name} deals ${attk.damage} damage.  ${attk.defender.name}'s hps are reduced to ${attk.defender.hps}`);
+    flashDamage(attk.defender.id);
+    reduceHps(attk.defender);
 }
 
+function animateWin(winner) {
+    console.log(`${winner.name} wins!`);
+    attackMessage(`${winner.name} wins!`);
+}
+
+function showGame(heroStats, villianStats) {
+
+    fillTable("hero", heroStats);
+    fillTable("villian", villianStats);
+    for (let e of document.getElementsByClassName("output")) {
+        e.style.visibility = "visible";
+    }
+}
+
+function fillTable(tableId, stats) {
+    // The names of the table elements match the names
+    // of the stats objects.
+    const table = document.getElementById(tableId);
+    for (let name in stats) {
+        let elements = table.getElementsByClassName(name);
+        // This will skip the 'id' field which doesn't apply and any additional that are added.
+        if (elements.length == 1) {
+            elements[0].innerHTML = stats[name];
+        }
+    }
+}
+
+function attackMessage(mssg) {
+    document.getElementById("attack-info").innerHTML = mssg;
+}
+
+// id: "hero" / "villian"
+function flashDamage(id) {
+    unflashDamage();
+    document.getElementById(id).style.backgroundColor = "red";
+}
+
+function unflashDamage() {
+    document.getElementById("hero").style.backgroundColor = "";
+    document.getElementById("villian").style.backgroundColor = "";
+}
+
+function reduceHps(defender) {
+    const element = document
+        .getElementById(defender.id)
+        .getElementsByClassName("hps")[0];
+    element.innerHTML = defender.hps;
+}
+
+function extractStats() {
+    var formElement = document.querySelector("form");
+    var formData = new FormData(formElement);
+    const data = Object.fromEntries(formData.entries());
+    data.id = "hero";
+    return data;
+}
+
+// Custom 'oninput' functionality for the html range sliders
 function range(name) {
     const sliders = document.getElementsByClassName('slider');
     // Subtract all the slider values from the remaining pool except the one
     // that has been updated.  Hang on the thisSlider.
-    var remaining = TROOPS;
+    var remaining = POOL;
     var thisSlider;
-    for (slider of sliders) {
+    for (let slider of sliders) {
         if (slider.name != name) {
             remaining -= parseInt(slider.value);
         } else {
@@ -110,40 +223,35 @@ function range(name) {
     document.getElementById(name).innerHTML = thisSlider.value;
 
     // Now set the max on all sliders to their current value + remaining
-    for (slider of sliders) {
+    for (let slider of sliders) {
         slider.max = parseInt(slider.value) + remaining;
     }
     document.getElementById("pool").innerHTML = remaining;
 }
 
-function extractStats() {
-    var formElement = document.querySelector("form");
-    var formData = new FormData(formElement);
-    const data = Object.fromEntries(formData.entries());
-    return data;
+
+//-------------------------------- utils ----------------------------------------
+
+function flipCoin() {
+    if (Math.random() > 0.5) {
+        return 0;
+    }
+    return 1;
 }
 
-function getOpponentStats() {
-    return randomFoe();
-}
-
-function randomFoe() {
-    const output = {name: "foe"};
-    // We need a min of 1 for attack and hps.
-    var troops = TROOPS;
-    output.attack = getRandomInt(1, troops - 1); // we need 1 for hps
-    troops -= output.attack;
-    output.hps = getRandomInt(1, troops);
-    troops -= output.hps;
-    output.defense = getRandomInt(0, troops);
-    troops -= output.defense;
-    output.speed = troops;
+// Return a settimeout function that increments its
+// timeout every call.  Optionally set an initial timeout
+// with the second parameter.  Otherwise the first call
+// happens immediately.
+function timeout(increment, initial) {
+    var t = (initial) ? initial : 0;
+    var output = function(callback, args) {
+        setTimeout(callback, t, ...args);
+        t += increment;
+    }
     return output;
 }
 
-function getRandomInt(min, max) {
-    // non inclusive
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+function clone(obj) {
+    return Object.assign({}, obj);
 }
